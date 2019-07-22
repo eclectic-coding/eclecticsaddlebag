d869163d4c5d5c64340e1554541475ebed5d3009
eclecticcoding <chuck@eclecticsaddlebag.com>
2019-07-22 12:58:32 -0400

    My initial Commit.

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
-import React from "react";
-import { Styled, jsx } from "theme-ui";
-import { Link } from "gatsby";
+import React from 'react'
+import { Styled, jsx } from 'theme-ui'
+import { Link } from 'gatsby'
 /**
  * Shadow me to add your own bio content
  */
@@ -9,15 +9,15 @@ import { Link } from "gatsby";
 export default () => (
   <div>
     <div sx={{ mb: 2 }}>
-      Words by <Styled.a href="http://example.com/">Jane Doe</Styled.a>.
+      Words by <Styled.a href='http://example.com/'>Jane Doe</Styled.a>.
       <br />
       Change me. This is all quite default. If you'd like.
     </div>
     <div sx={{ mb: 0 }}>
-      TODO: make these p elements once gatsby-theme-blog is fixed.
-      <Styled.a as={Link} to="/about">
+      {/* TODO: make these p elements once gatsby-theme-blog is fixed. */}
+      <Styled.a as={Link} to='/about'>
         More about me
       </Styled.a>
     </div>
   </div>
-);
+)
